dc0bc2d1e92defb5f2c4d1736f6d55e95c95dd94
Chris Thoburn <cthoburn@linkedin.com>
2017-02-08 01:22:04 -0800

    port from twiddle

--- app/app.js
+++ app/app.js
@@ -10,7 +10,26 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+  customEvents: {
+    mouseenter: null,
+    mouseleave: null,
+    mousemove: null,
+    mousedown: null,
+    mouseup: null,
+    focus: null,
+    blur: null,
+    focusin: null,
+    focusout: null,
+    click: null,
+    dragstart: null,
+    drag: null,
+    dragenter: null,
+    dragleave: null,
+    dragover: null,
+    dragend: null,
+    drop: null
+  }
 });
 
 loadInitializers(App, config.modulePrefix);
